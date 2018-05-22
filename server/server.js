c5d7d15aa5d5c5dff2f923b413de07f0d76aa48b
geekxia <448914712@qq.com>
2018-05-22 08:13:53 +0800

    0522

--- server/server.js
+++ server/server.js
@@ -3,15 +3,21 @@ const ReactSSR = require('react-dom/server');  // 用于服务端渲染的API
 // require默认引入的是整个模块exports出来的内容，所以要用.default
 const path = require('path');
 const fs = require('fs');
+const favicon = require('serve-favicon');  // 制作网站小图标
 
 const app = express();
 const isDev = process.env.NODE_ENV === 'development';
+
+app.use(favicon(path.join(__dirname, '../favicon.ico')))
 
 if (isDev) {
     // 开发环境时，没有dist目录
     // const devStatic = require('./util/devStatic');
     // devStatic(app);
     console.log('这是开发环境');
+    app.get('*', function(req, res) {
+        res.send('hello development');
+    })
 } else {
     app.use('/public', express.static(path.join(__dirname, '../dist')));
     let template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8');
